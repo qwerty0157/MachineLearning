2afc596e62426dcd81d6b423fe5566da174080df
qwerty0157 <qwerty0157@gmail.com>
2018-08-05 00:18:10 +0900

    add Machine learning function

---
+++ MachineLearning/MachineLearning/LoadData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using CsvHelper;
+
+namespace ML
+{
+    public static class LoadData
+    {
+        /// <summary>
+        /// Reads the csv.
+        /// </summary>
+        /// <returns>The csv.</returns>
+        /// <param name="filename">ファイル名</param>
+        /// <typeparam name="T">CSVのカラム情報.</typeparam>
+        /// <typeparam name="M">Mapper</typeparam>
+        public static List<T> LoadCsv<T, M>(string filename) where M : CsvHelper.Configuration.CsvClassMap<T>
+        {
+            //絶対ファイルパス
+            using (var parse = new CsvReader(new StreamReader(filename)))
+            {
+                parse.Configuration.HasHeaderRecord = false;
+                parse.Configuration.RegisterClassMap<M>();
+                List<T> data = parse.GetRecords<T>().ToList();
+                return data;
+            }
+        }
+    }
+
+    class Iris
+    {
+        public double SepalLength { get; set; }
+        public double SepalWidth { get; set; }
+        public double PetalLength { get; set; }
+        public double PetalWidth { get; set; }
+        public string Species { get; set; }
+
+    }
+
+    class IrisMap : CsvHelper.Configuration.CsvClassMap<Iris>
+    {
+        public IrisMap()
+        {
+            Map(x => x.SepalLength).Index(0);
+            Map(x => x.SepalWidth).Index(1);
+            Map(x => x.PetalLength).Index(2);
+            Map(x => x.PetalWidth).Index(3);
+            Map(x => x.Species).Index(4);
+        }
+    }
+}
--- MachineLearning/MachineLearning/Program.cs
+++ MachineLearning/MachineLearning/Program.cs
@@ -1,12 +1,90 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using CsvHelper;
 
-namespace MachineLearning
+namespace ML
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var data = LoadData.LoadCsv<Iris, IrisMap>("../../../../TestData/IRIS_Test.csv");
+            var labelname = data.Select(x => x.Species).Distinct().ToArray();
+
+            var feature = (from n in data select new double[] { n.PetalLength, n.PetalWidth, n.SepalLength, n.SepalWidth }).ToArray();
+            var label = data.Select(x => Array.IndexOf(labelname, x.Species)).ToArray();
+
+            var train = feature.Select((x, i) => new { Content = x, Index = i })
+                               .Where(x => x.Index < 40 ||
+                                      (x.Index >= 50 && x.Index < 90) ||
+                                      (x.Index >= 100 && x.Index < 140))
+                               .Select(x => x.Content)
+                               .ToArray();
+            var trainl = label.Select((x, i) => new { Content = x, Index = i })
+                              .Where(x => x.Index < 40 ||
+                                     (x.Index >= 50 && x.Index < 90) ||
+                                     (x.Index >= 100 && x.Index < 140))
+                              .Select(x => x.Content)
+                              .ToArray(); ;
+            var test = feature.Select((x, i) => new { Content = x, Index = i })
+                              .Where(x => (x.Index >= 40 && x.Index < 50) ||
+                                     (x.Index >= 90 && x.Index < 100) ||
+                                      x.Index >= 140)
+                              .Select(x => x.Content)
+                              .ToArray();
+            var testl = label.Select((x, i) => new { Content = x, Index = i })
+                             .Where(x => (x.Index >= 40 && x.Index < 50) ||
+                                    (x.Index >= 90 && x.Index < 100) ||
+                                    x.Index >= 140)
+                             .Select(x => x.Content)
+                             .ToArray();
+
+            var svm = new SVM(train, trainl);
+            svm.learn();
+            var result = test.Select(x => svm.predict(x)).ToArray();
+            ClassificationReport(testl, result);
+        }
+        /// <summary>
+        /// Classifications the report.
+        /// </summary>
+        /// <param name="truelabel">正解ラベル</param>
+        /// <param name="predict">予測結果</param>
+        public static void ClassificationReport(int[] truelabel, int[] predict)
+        {
+            Debug.Assert(truelabel.Length != predict.Length);
+            var labelset = truelabel.Distinct().ToArray();
+            var tfarray = new bool[truelabel.Length];
+            double precision;
+            double recall;
+            for (int i = 0; i < truelabel.Length; i++)
+            {
+                tfarray[i] = truelabel[i] == predict[i];
+            }
+            Console.WriteLine("label\tPre\tRec\tF-score");
+            foreach (var item in labelset)
+            {
+                // ラベルがitemのもののindexを正解ラベルからとってくる
+                var preIndex = truelabel.Select((x, i) => new { Content = x, Index = i })
+                                        .Where(x => x.Content == item)
+                                        .Select(x => x.Index).ToArray();
+                var recIndex = predict.Select((x, i) => new { Content = x, Index = i })
+                                      .Where(x => x.Content == item)
+                                      .Select(x => x.Index).ToArray();
+                precision = tfarray.Select((x, i) => new { Content = x, Index = i })
+                                   .Count(x => 0 <= Array.IndexOf(preIndex, x.Index) && x.Content == true)
+                                   / (double)preIndex.Length;
+                recall = tfarray.Select((x, i) => new { Content = x, Index = i })
+                                .Count(x => 0 <= Array.IndexOf(recIndex, x.Index) && x.Content == true)
+                                / (double)recIndex.Length;
+                Console.WriteLine("{0}\t{1:f2}\t{2:f2}\t{3:f2}", item, precision, recall,
+                                  (2 * precision * recall) / (precision + recall));
+
+            }
         }
     }
-}
